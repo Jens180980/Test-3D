39da0f28c1bad6db04368f8f77ccf71b544e8ffe
Jens Harder <76961272+Jens180980@users.noreply.github.com>
2023-02-05 19:06:05 +0100

    before wheel faceUV

--- src/index.js
+++ src/index.js
@@ -190,10 +190,44 @@ const buildCar = () => {
   outline.push(new BABYLON.Vector3(0, 0, 0.1));
   outline.push(new BABYLON.Vector3(-0.3, 0, 0.1));
 
+  const carMat = new BABYLON.StandardMaterial("carMat");
+  carMat.diffuseTexture = new BABYLON.Texture(
+    "https://assets.babylonjs.com/environments/car.png"
+  );
+
+  const faceUV = [];
+  faceUV[0] = new BABYLON.Vector4(0, 0.5, 0.38, 1);
+  faceUV[1] = new BABYLON.Vector4(0, 0, 1, 0.5);
+  faceUV[2] = new BABYLON.Vector4(0.38, 1, 0, 0.5);
+
   const car = BABYLON.MeshBuilder.ExtrudePolygon("car", {
     shape: outline,
     depth: 0.2,
+    faceUV: faceUV,
+    wrap: true,
   });
+
+  car.material = carMat;
+
+  car.position.y = 0.15;
+  car.rotation.x = -Math.PI / 2;
+
+  const wheelRB = BABYLON.MeshBuilder.CreateCylinder("wheelRB", {
+    diameter: 0.125,
+    height: 0.05,
+  });
+  wheelRB.parent = car;
+  wheelRB.position = new BABYLON.Vector3(-0.2, 0.035, -0.1);
+
+  const wheelRF = wheelRB.clone("wheelRF");
+  wheelRF.position.x = 0.1;
+
+  const wheelLB = wheelRB.clone("wheelLB");
+  wheelLB.position.y = -0.2 - 0.035;
+
+  const wheelLF = wheelRB.clone("wheelLF");
+  wheelLF.position.y = -0.2 - 0.035;
+  wheelLF.position.x = 0.1;
 }; // End buildCar Fn
 
 // *****************************************************************
